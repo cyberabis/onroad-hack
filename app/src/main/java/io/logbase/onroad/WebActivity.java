fcd4a8e52d26ce6ece32d9a21f5fe078e79e7363
cyberabis <babishek@gmail.com>
2015-05-09 20:32:21 +0530

    updates

--- app/src/main/java/io/logbase/onroad/WebActivity.java
+++ app/src/main/java/io/logbase/onroad/WebActivity.java
@@ -1,5 +1,6 @@
 package io.logbase.onroad;
 
+import android.app.Activity;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
@@ -9,7 +10,7 @@ import android.webkit.WebSettings;
 import android.webkit.WebView;
 
 
-public class WebActivity extends ActionBarActivity {
+public class WebActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,26 +24,4 @@ public class WebActivity extends ActionBarActivity {
         myWebView.loadUrl("http://192.168.24.111:8000");
     }
 
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_web, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
 }
